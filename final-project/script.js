dfc38489b155b9e5d2cdbedd90f2606952af8c6b
Sani Abubakar Adam <Athernee@gmail.com>
2025-07-31 17:55:24 +0100

    fix: show only one timer at a time

--- final-project/script.js
+++ final-project/script.js
@@ -1,9 +1,16 @@
+// function toggleTimer(timerId) {
+//   const element = document.getElementById(timerId);
+//   const isHidden = getComputedStyle(element).display === 'none';
+//   if (isHidden) {
+//     element.style.display = 'block';
+//   } else {
+//     element.style.display = 'none';
+//   }
+// }
+
 function toggleTimer(timerId) {
-  const element = document.getElementById(timerId);
-  const isHidden = getComputedStyle(element).display === 'none';
-  if (isHidden) {
-    element.style.display = 'block';
-  } else {
-    element.style.display = 'none';
-  }
+  document.querySelectorAll('.durations')
+    .forEach(el => el.style.display = 'none');
+  document.getElementById(timerId).style.display = 'block';
+
 }
