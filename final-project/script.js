5e312c2dc6bdca32458f721d5063d2e5c31fc46d
Sani Abubakar Adam <Athernee@gmail.com>
2025-08-02 16:26:50 +0100

    feat: implement start timer func.

--- final-project/script.js
+++ final-project/script.js
@@ -9,10 +9,44 @@ function toggleTimer(timerId, btn) {
     .forEach(b => b.classList.remove('active'));
   btn.classList.add('active');
 
-  return activeTimer;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   const first = document.querySelector('.tab-btn');
   if (first) first.click();
 });
+
+const intervals = {};
+
+function startTimer(timerId) {
+  if (intervals[timerId]) {
+    clearInterval(intervals[timerId]);
+  }
+
+  const activeTimer = document.getElementById(timerId);
+
+  const minutes = activeTimer.querySelector('.minutes');
+  const seconds = activeTimer.querySelector('.seconds');
+
+  // parse once when started
+  let parseMinutes = parseInt(minutes.textContent, 10);
+  let parseSeconds = parseInt(seconds.textContent, 10);
+
+  // store interval ID
+  intervals[timerId] = setInterval(() => {
+    if (parseSeconds === 0) {
+      if (parseMinutes === 0) {
+        clearInterval(intervals[timerId]);
+        return;
+      }
+      parseMinutes--;
+      parseSeconds = 59;
+    } else {
+      parseSeconds--;
+    }
+
+    minutes.textContent = String(parseMinutes).padStart(2, '0')
+    seconds.textContent = String(parseSeconds).padStart(2, '0');
+  }, 1000);
+
+}
